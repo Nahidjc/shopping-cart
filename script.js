272b22093f3fe1d0788cb52d1f93e22f415aa3a1
Nahidjc <nahidjc80@gmail.com>
2021-06-28 16:07:00 +0600

    design memo

--- script.js
+++ script.js
@@ -104,4 +104,31 @@ document.getElementById("removeItem2").addEventListener('click', function () {
 
 document.getElementById("checkoutbtn").addEventListener('click', function () {
     parentItem = document.getElementById("shoppingcart").style.display = "none";
+    document.getElementById("formPortion").style.display = "block";
+})
+
+document.getElementById("submitButton").addEventListener('click', function () {
+    parentItem = document.getElementById("formPortion").style.display = "none";
+    document.getElementById("orderDetails").style.display = "block";
+
+    totalPriceItem1 = document.getElementById("priceOfItem1").innerText;
+    totalPriceItem2 = document.getElementById("priceOfItem2").innerText;
+
+    totalQuantityItem1 = document.getElementById("quantityOfItem1").value;
+    totalQuantityItem2 = document.getElementById("quantityOfItem2").value;
+
+    document.getElementById("setAmountItem1").innerText = totalPriceItem1;
+    document.getElementById("setAmountItem2").innerText = totalPriceItem2;
+
+    document.getElementById("setQuantityItem1").innerHTML = totalQuantityItem1;
+    document.getElementById("setQuantityItem2").innerHTML = totalQuantityItem2;
+
+    subtotal = document.getElementById("subtotal").innerText;
+    tax = document.getElementById("tax").innerText;
+    total = document.getElementById("total").innerText;
+
+    document.getElementById("setSubTotal").innerHTML = subtotal;
+    document.getElementById("setTax").innerHTML = tax;
+    document.getElementById("setTotal").innerHTML = total;
+
 })
